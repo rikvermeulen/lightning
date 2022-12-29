947e5c0f8fbb58d2342442dabaf781adc712726d
Rik Vermeulen <rik.vermeulen.1997@live.nl>
2022-12-29 11:16:19 +0100

    added config for jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "." });
+
+const customJestConfig = {
+  testEnvironment: "jsdom",
+  clearMocks: true,
+  moduleDirectories: ["node_modules", "app"],
+  setupFilesAfterEnv: ["<rootDir>/app/setupTests.ts"],
+  testRegex: "(/__tests__/.*|(\\.|/)test)\\.[jt]sx?$",
+};
+
+module.exports = createJestConfig(customJestConfig);
