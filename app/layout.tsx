0fffeb5693f46ca0d3a1d6e1b36062132d459aa5
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-01-14 15:23:34 +0100

    intergrated @next/font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import '../public/styles/global.css';
 
+import { Inter } from '@next/font/google';
+
+/*
+    @next/font will automatically optimize your fonts (including custom fonts) and remove 
+    external network requests for improved privacy and performance.
+    Find out more at https://beta.nextjs.org/docs/optimizing/fonts
+ */
+
+const inter = Inter({
+  variable: '--font-inter',
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${inter.variable}`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
