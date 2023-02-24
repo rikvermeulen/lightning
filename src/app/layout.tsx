0a2fe346cfe0cf0441a2c4038ac26a18d2a251ce
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-02-24 14:47:39 +0100

    feat: added new meta api from @next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import '../styles/global.css';
 
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import Header from './components/Header';
+import { meta } from './meta';
 
 /*
     @next/font will automatically optimize your fonts (including custom fonts) and remove 
     external network requests for improved privacy and performance.
     Find out more at https://beta.nextjs.org/docs/optimizing/fonts
  */
-
 const inter = Inter({
   variable: '--font-inter',
   subsets: ['latin'],
 });
+
+export async function generateMetadata(): Promise<Metadata> {
+  return meta;
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Welcome to Lightning',
+};
 
 export default function Home() {
   return (
