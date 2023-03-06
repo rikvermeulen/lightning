ce28e65381f452c1161b7b3f360316c9eb51c631
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-06 10:41:19 +0100

    feat: moved config prettier to config file

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 100,
+  tailwindConfig: './tailwind.config.js',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  pluginSearchDirs: false,
+  exludeFiles: ['node_modules/**', 'dist/**', '.next/**'],
+};
+
+module.exports = config;
