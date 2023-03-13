b9d10cbb9c8706e9f37b55b644a6cbc11b1b0fbb
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-13 16:22:18 +0100

    fix: tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
       <body>
         <Header />
         {children}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
-    '.src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
