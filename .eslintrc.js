7d264fb1d8390c8580b5a3c83c3ed0d1f8b7541b
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-03-27 10:33:29 +0200

    fix: eslint config to override exclude files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,50 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+    'unused-imports',
+    'tailwindcss',
+    'simple-import-sort',
+    'prettier',
+  ],
   extends: [
     'airbnb-typescript',
     'next/core-web-vitals',
     'plugin:prettier/recommended',
     'plugin:import/errors',
-    'plugin:import/warnings'
+    'plugin:import/warnings',
   ],
-  plugins: [
-    '@typescript-eslint', 
-    'unused-imports', 
-    'tailwindcss', 
-    'simple-import-sort', 
-    'prettier',
-  ],
-  ignorePatterns: [
-   'out/*', 
-  'node_modules/*',
-  '.eslintrc.js',
-  'next.config.js',
-  'next-sitemap.config.js',
-  'lint-staged.config.js',
-  '.prettierrc.cjs'],
-  parserOptions: {
-    parser: '@typescript-eslint/parser',
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-  },
-  rules: {
-    'prettier/prettier': [
-      "error",
-      {
-        "singleQuote": true,
-        "endOfLine": "auto"
-      }
-    ],
-    '@next/next/no-html-link-for-pages': ['off']
-  },
-  settings: {
-    'import/resolver': {
-      typescript: {
+  ignorePatterns: ['out/*', 'node_modules/*', '.eslintrc.js', 'next.config.js'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parserOptions: {
         project: './tsconfig.json',
-        alwaysTryTypes: true,
+        tsconfigRootDir: __dirname,
+      },
+      rules: {
+        'prettier/prettier': [
+          'error',
+          {
+            singleQuote: true,
+            endOfLine: 'auto',
+          },
+        ],
+        '@next/next/no-html-link-for-pages': ['off'],
+      },
+      settings: {
+        'import/resolver': {
+          typescript: {
+            project: './tsconfig.json',
+            alwaysTryTypes: true,
+          },
+        },
+      },
+      env: {
+        browser: true,
+        node: true,
+        es2021: true,
       },
     },
-  },
-  env: {
-    browser: true,
-    node: true,
-    es2021: true,
-  },
+  ],
 };
