69e4e80fdbc5d01b1cf3968c8e1a4fa2c5f9ac21
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-04-28 15:36:00 +0200

    feat: seprate lib util for fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,11 @@
 import '@/styles/global.css';
-
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 
-import Header from '@/components/Header';
-import meta from '@/app/meta';
 import type { RootLayoutProps } from '@/types/index';
 
-/**
- * @next/font will optimize your fonts and remove external network requests
- * @see https://beta.nextjs.org/docs/optimizing/fonts
- */
-
-const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
-});
+import { fontSans } from '@/lib/fonts';
+import Header from '@/components/Header';
+import meta from '@/app/meta';
 
 /**
  * generateMetadata is a function that returns a promise of Metadata.
@@ -30,7 +20,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className={`${inter.variable}`}>
+    <html lang="en" className={`${fontSans.variable}`}>
       <body>
         <div className="fixed -z-0 h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
         <Header />
