912257e0a62dfb9b19873968c5a9b5a5235dfb93
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-11 13:33:57 +0200

    perf: updated packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/styles/global.css';
+
 import type { Metadata } from 'next';
 
 import type { RootLayoutProps } from '@/types/index';
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,5 +10,4 @@ export default {
       },
     },
   },
-  plugins: [require('prettier-plugin-tailwindcss')],
 } satisfies Config;
