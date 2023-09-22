9c5eb327a825d19ba7ccfc9b5efdc14da3effb67
rikvermeulen <rik.vermeulen.1997@live.nl>
2023-09-22 12:31:09 +0200

    fix: response on main route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -5,6 +5,8 @@
  * @returns
  */
 
+import { NextResponse } from 'next/server';
+
 export async function GET(request: Request) {
-  return request;
+  return NextResponse.json({ hello: 'world' });
 }
